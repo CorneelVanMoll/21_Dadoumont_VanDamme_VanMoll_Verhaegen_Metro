57f827f71098cde401309521e6fa7132667026de
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2022-12-05 16:47:22 +0100

    First implementation of Metrocard

--- src/model/Metrocard.java
+++ src/model/Metrocard.java
@@ -1,4 +1,26 @@
 package model;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Metrocard {
+    private int id = 0;
+    Map<Integer, Metrocard> Metrocards;
+
+    public Metrocard() {
+        Metrocards = new TreeMap<>();
+    }
+
+    public void addMetrocard(Metrocard metrocard) {
+        if (metrocard == null) throw new IllegalArgumentException();
+        Metrocards.put(generateId(), metrocard);
+    }
+
+    public Map<Integer, Metrocard> getMetrocards(){
+        return Metrocards;
+    }
+
+    private int generateId(){
+        return ++id;
+    }
 }
