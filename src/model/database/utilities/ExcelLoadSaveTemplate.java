1784f1dcd32a45be4c0c3803adfb833ee54a3ff2
WoutVanDamme <91263011+WoutVanDamme@users.noreply.github.com>
2022-12-05 16:30:55 +0100

    load save

--- src/model/database/utilities/ExcelLoadSaveTemplate.java
+++ src/model/database/utilities/ExcelLoadSaveTemplate.java
@@ -1,4 +1,85 @@
 package model.database.utilities;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import jxl.read.biff.BiffException;
+import jxl.write.*;
+
 public class ExcelLoadSaveTemplate {
+
+    private static final String ExcelFile = "out.xls";
+
+    private static HashMap<Integer,String[]> data = new HashMap<>();
+
+    public static void main(String[] args) {
+        load();
+    }
+
+    public static void save() {
+        ExcelPlugin excelPlugin = new ExcelPlugin();
+
+        ArrayList<ArrayList<String>> out = new ArrayList<>();
+
+        for (Map.Entry<Integer, String[]> set : data.entrySet()) {
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add(set.getKey().toString());
+            for(String val: set.getValue()) {
+                temp.add(val);
+            }
+            out.add(temp);
+        }
+
+
+
+        try {
+            excelPlugin.write(new File(ExcelFile), out);
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WriteException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void load() {
+        ExcelPlugin excelPlugin = new ExcelPlugin();
+
+        try {
+            ArrayList<ArrayList<String>> out = excelPlugin.read(new File(ExcelFile));
+            data = new HashMap<>();
+            for(ArrayList<String> i : out) {
+                int count = 0;
+                int id=-1;
+                String[] values = new String[3];
+
+                for(String word: i) {
+                    if(count == 0) {
+                        id = Integer.valueOf(word);
+                    }else{
+                        values[count-1]=word;
+                    }
+
+                    count++;
+                }
+
+                data.put(id,values);
+            }
+
+
+        } catch (BiffException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- src/model/database/utilities/TekstLoadSaveTemplate.java
+++ src/model/database/utilities/TekstLoadSaveTemplate.java
@@ -1,4 +1,57 @@
 package model.database.utilities;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class TekstLoadSaveTemplate {
+
+
+    private static final String fileName = "metrocards.txt";
+    private static final String savetest = "savetest.txt";
+
+    private static HashMap<Integer,String[]> data = new HashMap<>();
+
+
+    public static void save() {
+
+        String out = "";
+
+        for (Map.Entry<Integer, String[]> set : data.entrySet()) {
+            out += set.getKey();
+            for(String val: set.getValue()) {
+                out += ";"+val;
+            }
+            out += '\n';
+        }
+        try {
+            FileWriter myWriter = new FileWriter(savetest);
+            myWriter.write(out);
+            myWriter.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public static void load() {
+        data = new HashMap<>();
+        try {
+            File myObj = new File(fileName);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String[] line = myReader.nextLine().split(";");
+                data.put(Integer.valueOf(line[0]), Arrays.copyOfRange(line,1,line.length));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
