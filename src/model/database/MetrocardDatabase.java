b5ab51e1b8bfe5a26efb81ee269181bb24868ef8
Samaoo <sam.verhaegen2003@gmail.com>
2022-12-12 16:26:46 +0100

    database + loadsavestrategy

--- src/model/database/MetrocardDatabase.java
+++ src/model/database/MetrocardDatabase.java
@@ -1,4 +1,33 @@
 package model.database;
 
+import model.Metrocard;
+import model.database.loadSaveStrategies.LoadSaveStrategy;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class MetrocardDatabase {
+    private TreeMap<Integer, Metrocard> metrocards;
+
+    private LoadSaveStrategy<Integer, Metrocard> loadSaveStrategy;
+
+    public void load() {
+        metrocards = new TreeMap<>(loadSaveStrategy.load());
+    }
+
+    public void save() {
+        loadSaveStrategy.save(metrocards);
+    }
+
+    public ArrayList<Metrocard> getMetrocardList() {
+        return new ArrayList<>(metrocards.values());
+    }
+
+    public ArrayList<Integer> getMetrocardIDList() {
+        return new ArrayList<>(metrocards.keySet());
+    }
+
+    public void setLoadSaveStrategy(LoadSaveStrategy loadSaveStrategy) {
+        this.loadSaveStrategy = loadSaveStrategy;
+    }
 }
--- src/model/database/loadSaveStrategies/LoadSaveStrategy.java
+++ src/model/database/loadSaveStrategies/LoadSaveStrategy.java
@@ -1,4 +1,8 @@
 package model.database.loadSaveStrategies;
 
-public interface LoadSaveStrategy {
+import java.util.Map;
+
+public interface LoadSaveStrategy<K,V> {
+    Map<K, V> load();
+    void save(Map<K, V> data);
 }
