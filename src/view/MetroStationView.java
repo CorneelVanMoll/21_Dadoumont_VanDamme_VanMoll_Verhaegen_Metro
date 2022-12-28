e33f1527e728984fac0994e23e16a54b12cf544c
WoutVanDamme <91263011+WoutVanDamme@users.noreply.github.com>
2022-12-28 15:50:15 +0100

    hhhhh
    hhhhhhhh

--- src/view/MetroStationView.java
+++ src/view/MetroStationView.java
@@ -1,58 +1,74 @@
 package view;
 
 import controller.MetroStationViewController;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import model.Gate;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MetroStationView {
 	MetroStationViewController metroStationViewController;
 
-	private Stage stage = new Stage();		
-	private Group root;
-	private HBox mainBox;
+	private final Stage stage = new Stage();
+
+	private List<Integer> metroCardIDList;
+	private final List<ComboBox<Integer>> cbxCardIDsList;
 
 	private HashMap<Gate,TextField> outputs;
 
+	private HBox rootHBox;
 
-	public MetroStationView(){			
+	public MetroStationView(){
+		Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
 		stage.setTitle("METRO STATION VIEW");
 		stage.initStyle(StageStyle.UTILITY);
-		stage.setX(5);
-		stage.setY(390);
-		root = new Group();
-		mainBox = new HBox();
-		mainBox.setSpacing(10);
-		root.getChildren().add(mainBox);
+		stage.setX(0);
+		stage.setY(bounds.getMinY() + (bounds.getHeight() / 2));
 
-		Scene scene = new Scene(root, 650, 300);
+		cbxCardIDsList = new ArrayList<>();
 
 
+		outputs = new HashMap<>();
+
+
+
+		// Root Hbox
+		rootHBox = new HBox(10);
+		rootHBox.setPadding(new Insets(10));
+
+
+		Scene scene = new Scene(rootHBox, bounds.getWidth() / 2, (bounds.getHeight() / 2));
 		stage.setScene(scene);
-		stage.sizeToScene();			
-		stage.show();		
+		stage.sizeToScene();
+		stage.show();
+	}
+
+	public void updateMetroCardIdList(List<Integer> IDs) {
+		this.metroCardIDList = IDs;
+		for (ComboBox<Integer> cbx : cbxCardIDsList) {
+			cbx.getItems().setAll(metroCardIDList);
+		}
+
+
 	}
 
 	public void setMetroStationViewController(MetroStationViewController metroStationViewController) {
 		this.metroStationViewController = metroStationViewController;
-		outputs = new HashMap<>();
+
+
 
 		for(Gate gate: metroStationViewController.getGates()) {
 			TextField output = new TextField();
@@ -60,47 +76,52 @@ public class MetroStationView {
 			outputs.put(gate,output);
 		}
 
+
+
+
 		for(Gate gate: metroStationViewController.getGates()) {
 
+			VBox gate1VBox = new VBox(10);
+			gate1VBox.setPadding(new Insets(5));
+			gate1VBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY,Insets.EMPTY)));
+			gate1VBox.setBorder(new Border(new BorderStroke(Color.valueOf("#9E9E9E"),
+					BorderStrokeStyle.SOLID,
+					CornerRadii.EMPTY,
+					BorderWidths.DEFAULT)));
+			rootHBox.getChildren().add(gate1VBox);
 
 
-			HBox hbox = new HBox();
+			Label gate1Label = new Label(gate.getName());
+			gate1VBox.getChildren().add(gate1Label);
 
 
-			hbox.setAlignment(Pos.BASELINE_CENTER);
-			hbox.setPadding(new Insets(15, 12, 15, 12));
-			hbox.setSpacing(10);
-			hbox.setStyle("-fx-background-color: #336699;");
+			Label gate1MetroCardIDLabel = new Label("Metrocard ID:");
+			gate1VBox.getChildren().add(gate1MetroCardIDLabel);
 
 
+			ComboBox<Integer> gate1IDComboBox = new ComboBox<>();
+			gate1IDComboBox.setItems(metroStationViewController.getIDs());
+			cbxCardIDsList.add(gate1IDComboBox);
+			gate1VBox.getChildren().add(gate1IDComboBox);
 
-
-			Label label = new Label(gate.getName());
-
-			final ComboBox comboBox = new ComboBox(metroStationViewController.getIDs());
-
-
-			Button scanBtn = new Button("Scan metrocard");
-			scanBtn.setPrefSize(100, 20);
-			scanBtn.setOnAction((event) -> {
-				this.metroStationViewController.scanMetroCard(gate, comboBox.getValue().toString());
+			// Gate 1 Scan Metro Card Button
+			Button gate1ScanButton = new Button("Scan metro card");
+			gate1ScanButton.setOnAction((event) -> {
+				this.metroStationViewController.scanMetroCard(gate, gate1IDComboBox.getValue().toString());
 
 			});
+			gate1VBox.getChildren().add(gate1ScanButton);
 
-
-			Button walkBtn = new Button("Walk through gate");
-			walkBtn.setPrefSize(100, 20);
-			walkBtn.setOnAction((event) -> {
+			// Gate 1 Walk through Gate Button
+			Button gate1WalkButton = new Button("Walk through gate");
+			gate1WalkButton.setOnAction((event) -> {
 
 				this.metroStationViewController.walkThroughGate(gate);
 
 			});
+			gate1VBox.getChildren().add(gate1WalkButton);
 
-
-
-			hbox.getChildren().addAll(label,comboBox,scanBtn,walkBtn, outputs.get(gate));
-
-			mainBox.getChildren().add(hbox);
+			gate1VBox.getChildren().add(outputs.get(gate));
 		}
 
 	}
