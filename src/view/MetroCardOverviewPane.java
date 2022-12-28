078bb74d54df2b04b60c8c912be509b2c20a64e7
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2022-12-28 17:02:50 +0100

    Metro Card Table full width, columns 25% of width each

--- src/view/MetroCardOverviewPane.java
+++ src/view/MetroCardOverviewPane.java
@@ -2,40 +2,69 @@ package view;
 
 import controller.MetroCardOverviewPaneController;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import model.Metrocard;
 
 import java.util.List;
 
-public class MetroCardOverviewPane extends GridPane {
+public class MetroCardOverviewPane extends Pane {
     private MetroCardOverviewPaneController metroCardOverviewPaneController;
 
     private TableView<Metrocard> table;
 
     public MetroCardOverviewPane() {
-        this.setPadding(new Insets(5, 5, 5, 5));
-        this.setVgap(5);
-        this.setHgap(5);
-        this.add(new Label("List of Metro cards:"), 0, 0, 1, 1);
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
 
+        // Root VBox
+        VBox rootVBox = new VBox(10);
+        rootVBox.setPadding(new Insets(10));
+        rootVBox.setPrefWidth(bounds.getWidth() / 2);
+
+        // List Metro Cards Label
+        Label listMetroCardLabel = new Label("List of metro cards:");
+        rootVBox.getChildren().add(listMetroCardLabel);
+
+        // Table Metro Cards
         this.table = new TableView<>();
+        table.setPrefWidth(rootVBox.getPrefWidth() - rootVBox.getPadding().getLeft()
+        - rootVBox.getPadding().getRight());
 
         TableColumn<Metrocard, Integer> id = new TableColumn<>("ID");
         id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        TableColumn<Metrocard, String> month = new TableColumn<>("Available until");
-        month.setCellValueFactory(new PropertyValueFactory<>("date"));
+        id.prefWidthProperty().bind(table.prefWidthProperty().multiply(0.25));
+
+        TableColumn<Metrocard, String> date = new TableColumn<>("Date bought");
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        date.prefWidthProperty().bind(table.prefWidthProperty().multiply(0.25));
+
         TableColumn<Metrocard, Integer> availableTrips = new TableColumn<>("Available trips");
         availableTrips.setCellValueFactory(new PropertyValueFactory<>("availableTrips"));
+        availableTrips.prefWidthProperty().bind(table.prefWidthProperty().multiply(0.25));
+
         TableColumn<Metrocard, Integer> usedTrips = new TableColumn<>("Used trips");
         usedTrips.setCellValueFactory(new PropertyValueFactory<>("usedTrips"));
+        usedTrips.prefWidthProperty().bind(table.prefWidthProperty().multiply(0.25));
 
-        table.getColumns().addAll(id, month, availableTrips, usedTrips);
+        table.getColumns().addAll(id, date, availableTrips, usedTrips);
 
-        this.add(this.table, 0, 1, 1, 1);
+
+
+
+
+        rootVBox.getChildren().add(table);
+
+
+        this.getChildren().add(rootVBox);
+
+
 
     }
 
