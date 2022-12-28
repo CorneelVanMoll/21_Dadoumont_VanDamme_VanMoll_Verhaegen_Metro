2ac451abfbf5ea305ad99000e8c017772d1c68d8
Samaoo <sam.verhaegen2003@gmail.com>
2022-12-28 22:03:12 +0100

    use excelplugin

--- src/model/database/utilities/ExcelPlugin.java
+++ src/model/database/utilities/ExcelPlugin.java
@@ -3,6 +3,7 @@ package model.database.utilities;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import jxl.*; 
 import jxl.read.biff.BiffException;
@@ -10,41 +11,37 @@ import jxl.write.Label;
 import jxl.write.WritableSheet;
 import jxl.write.WritableWorkbook;
 import jxl.write.WriteException;
-import jxl.write.biff.RowsExceededException;
 
 public class ExcelPlugin {
-	
-	public void write(File file, ArrayList<ArrayList<String>> args) 
-				throws BiffException, IOException, RowsExceededException, WriteException{
-	
+	public void write(File file, List<List<String>> args)
+				throws IOException, WriteException {
+
 		WritableWorkbook workbook = Workbook.createWorkbook(file); 
 		workbook.createSheet("sheet1", 0);
 		WritableSheet sheet = workbook.getSheet(0); 
-		for(int i = 0; i < args.size(); i++){
-			ArrayList<String> parameters = args.get(i);
-			for(int j = 0; j < parameters.size(); j++){
+		for (int i = 0; i < args.size(); i++) {
+			List<String> parameters = args.get(i);
+			for (int j = 0; j < parameters.size(); j++) {
 				Label label = new Label(j, i, parameters.get(j));
 				sheet.addCell(label); 
 			}
 		}
 		workbook.write();
 		workbook.close();
-				
 	}
 	
-	public ArrayList<ArrayList<String>> read(File file) 
+	public List<List<String>> read(File file)
 			throws BiffException, IOException {
 
 		Workbook workbook = Workbook.getWorkbook(file);
 		Sheet sheet = workbook.getSheet(0);
 		int row = 0;	
 		
-		ArrayList<ArrayList<String>> info = new ArrayList<ArrayList<String>>();
-		while(row < sheet.getRows())
-		{
+		List<List<String>> info = new ArrayList<>();
+		while (row < sheet.getRows()) {
 			int column = 0;
-			ArrayList<String> rowinfo = new ArrayList<String>();
-			while(column < sheet.getColumns()){
+			List<String> rowinfo = new ArrayList<>();
+			while (column < sheet.getColumns()) {
 				Cell cell = sheet.getCell(column,row);
 				String information = cell.getContents();
 				rowinfo.add(information);
@@ -56,5 +53,4 @@ public class ExcelPlugin {
 		workbook.close();
 		return info;
 	}
-
 }
