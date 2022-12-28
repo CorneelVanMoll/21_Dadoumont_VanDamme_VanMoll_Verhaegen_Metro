8b819910bce5e7cb783dc94b624fe018c29a6cbf
Samaoo <sam.verhaegen2003@gmail.com>
2022-12-28 18:45:34 +0100

    load loadsave strategy

--- src/model/TicketPriceDecorator/TicketPriceFactory.java
+++ src/model/TicketPriceDecorator/TicketPriceFactory.java
@@ -48,6 +48,4 @@ public class TicketPriceFactory {
         }
         return discounts;
     }
-
-
 }
--- src/model/database/loadSaveStrategies/LoadSaveStrategyFactory.java
+++ src/model/database/loadSaveStrategies/LoadSaveStrategyFactory.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
+import java.util.List;
 
 public class LoadSaveStrategyFactory<K, V> {
     public LoadSaveStrategy<K, V> createLoadSaveStrategy(LoadSaveStrategyEnum loadSaveStrategy) {
@@ -31,5 +32,16 @@ public class LoadSaveStrategyFactory<K, V> {
             e.printStackTrace();
         }
         return strategyString;
+    }
+
+    public static LoadSaveStrategyEnum loadLoadSaveStrategy() {
+        LoadSaveStrategyEnum loadSaveStrategy = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("settings.txt"));
+            loadSaveStrategy = LoadSaveStrategyEnum.valueOf(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return loadSaveStrategy;
     }
 }
